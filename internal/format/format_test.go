e74b5c631ea8061b89539b477a50dcfd2316a83f
Erkenbend <ad.game-adress@hotmail.fr>
2025-10-09 20:53:47 +0200

    feat: Add flag to lint command for JSON output format

--- internal/format/format_test.go
+++ internal/format/format_test.go
@@ -2,11 +2,28 @@ package format_test
 
 import (
 	"bytes"
+	"encoding/json"
+	"reflect"
 	"testing"
 
 	"github.com/Marcel2603/tfcoach/internal/format"
 	"github.com/Marcel2603/tfcoach/internal/types"
 	"github.com/hashicorp/hcl/v2"
+)
+
+var (
+	issues1 = []types.Issue{
+		{
+			File:    "main.tf",
+			Range:   rng("main.tf", 0, 1), // prints as line 1 due to +1
+			Message: `resource name must be "this"`,
+			RuleID:  "core.naming.require_this",
+		},
+	}
+	issues2 = []types.Issue{
+		{File: "a.tf", Range: rng("a.tf", 4, 7), Message: "m1", RuleID: "r1"},
+		{File: "b.tf", Range: rng("b.tf", 9, 2), Message: "m2", RuleID: "r2"},
+	}
 )
 
 func rng(file string, line0, col int) hcl.Range {
@@ -18,54 +35,163 @@ func rng(file string, line0, col int) hcl.Range {
 	}
 }
 
-func TestWriteIssues_Single(t *testing.T) {
-	issues := []types.Issue{
-		{
-			File:    "main.tf",
-			Range:   rng("main.tf", 0, 1), // prints as line 1 due to +1
-			Message: `resource name must be "this"`,
-			RuleID:  "core.naming.require_this",
-		},
+func TestWriteResults_TextSingle(t *testing.T) {
+	var buf bytes.Buffer
+	err := format.WriteResults(issues1, &buf, "text")
+	if err != nil {
+		t.Fatalf("Unexpected error: %v, want none", err)
 	}
 
-	var buf bytes.Buffer
-	format.WriteIssues(issues, &buf)
+	want := `main.tf:0:1: resource name must be "this" (core.naming.require_this)
+Summary:
+ Issues: 1
+`
 
-	want := "main.tf:0:1: resource name must be \"this\" (core.naming.require_this)\n"
 	if got := buf.String(); got != want {
 		t.Fatalf("mismatch:\n got: %q\nwant: %q", got, want)
 	}
 }
 
-func TestWriteIssues_Multiple(t *testing.T) {
-	issues := []types.Issue{
-		{File: "a.tf", Range: rng("a.tf", 4, 7), Message: "m1", RuleID: "r1"},
-		{File: "b.tf", Range: rng("b.tf", 9, 2), Message: "m2", RuleID: "r2"},
+func TestWriteResults_TextMultiple(t *testing.T) {
+	var buf bytes.Buffer
+	err := format.WriteResults(issues2, &buf, "text")
+	if err != nil {
+		t.Fatalf("Unexpected error: %v, want none", err)
 	}
 
-	var buf bytes.Buffer
-	format.WriteIssues(issues, &buf)
+	want := `a.tf:4:7: m1 (r1)
+b.tf:9:2: m2 (r2)
+Summary:
+ Issues: 2
+`
 
-	want := "" +
-		"a.tf:4:7: m1 (r1)\n" + // 4+1 = 5
-		"b.tf:9:2: m2 (r2)\n" // 9+1 = 10
 	if got := buf.String(); got != want {
 		t.Fatalf("mismatch:\n got:\n%s\nwant:\n%s", got, want)
 	}
 }
 
-func TestWriteSummary(t *testing.T) {
-	issues := []types.Issue{
-		{File: "a.tf", Range: rng("a.tf", 4, 7), Message: "m1", RuleID: "r1"},
-		{File: "b.tf", Range: rng("b.tf", 9, 2), Message: "m2", RuleID: "r2"},
+func TestWriteResults_JsonSingle(t *testing.T) {
+	var buf bytes.Buffer
+	err := format.WriteResults(issues1, &buf, "json")
+	if err != nil {
+		t.Fatalf("Unexpected error: %v, want none", err)
 	}
 
+	want := `{
+  "IssueCount": 1,
+  "Issues": [
+	{
+	  "RuleID": "core.naming.require_this",
+      "Message": "resource name must be \"this\"",
+	  "File": "main.tf",
+	  "Range": {
+		"Filename": "main.tf",
+		"Start": {
+		  "Line": 0,
+		  "Column": 1,
+		  "Byte": 0
+		},
+		"End": {
+		  "Line": 0,
+		  "Column": 1,
+		  "Byte": 0
+		}
+	  }
+	}
+  ]
+}
+`
+
+	var gotJ, wantJ interface{}
+
+	if err = json.Unmarshal([]byte(want), &wantJ); err != nil {
+		t.Fatalf("Unexpected unmarshalling error in test setup: %v, want none", err)
+	}
+
+	got := buf.Bytes()
+	if err = json.Unmarshal(got, &gotJ); err != nil {
+		t.Fatalf("Unexpected unmarshalling error: %v, want none", err)
+	}
+
+	if !reflect.DeepEqual(gotJ, wantJ) {
+		t.Fatalf("JSON DeepEqual mismatch:\n got:\n%s\nwant:\n%s", got, want)
+	}
+}
+
+func TestWriteResults_JsonMultiple(t *testing.T) {
 	var buf bytes.Buffer
-	format.WriteSummary(issues, &buf)
+	err := format.WriteResults(issues2, &buf, "json")
+	if err != nil {
+		t.Fatalf("Unexpected error: %v, want none", err)
+	}
 
-	want := "Summary:\n" + " Issues: 2\n"
+	want := `{
+  "IssueCount": 2,
+  "Issues": [
+	{
+	  "File": "a.tf",
+	  "Range": {
+		"Filename": "a.tf",
+		"Start": {
+		  "Line": 4,
+		  "Column": 7,
+		  "Byte": 0
+		},
+		"End": {
+		  "Line": 4,
+		  "Column": 7,
+		  "Byte": 0
+		}
+	  },
+	  "Message": "m1",
+	  "RuleID": "r1"
+	},
+	{
+	  "File": "b.tf",
+	  "Range": {
+		"Filename": "b.tf",
+		"Start": {
+		  "Line": 9,
+		  "Column": 2,
+		  "Byte": 0
+		},
+		"End": {
+		  "Line": 9,
+		  "Column": 2,
+		  "Byte": 0
+		}
+	  },
+	  "Message": "m2",
+	  "RuleID": "r2"
+	}
+  ]
+}`
+	var gotJ, wantJ interface{}
 
-	if got := buf.String(); got != want {
-		t.Fatalf("mismatch:\n got:\n%s\nwant:\n%s", got, want)
+	if err = json.Unmarshal([]byte(want), &wantJ); err != nil {
+		t.Fatalf("Unexpected unmarshalling error in test setup: %v, want none", err)
+	}
+
+	got := buf.Bytes()
+
+	if err = json.Unmarshal(got, &gotJ); err != nil {
+		t.Fatalf("Unexpected unmarshalling error: %v, want none", err)
+	}
+
+	if !reflect.DeepEqual(gotJ, wantJ) {
+		t.Fatalf("JSON DeepEqual mismatch:\n got:\n%s\nwant:\n%s", got, want)
+	}
+}
+
+func TestWriteResults_UnknownFormat(t *testing.T) {
+	var buf bytes.Buffer
+	err := format.WriteResults(issues1, &buf, "abcd")
+	if err == nil {
+		t.Fatalf("Expected error, got none")
+	}
+
+	want := "unknown output format: abcd"
+	if err.Error() != want {
+		t.Fatalf("mismatch:\n got: %q\nwant: %q", err, want)
 	}
 }
