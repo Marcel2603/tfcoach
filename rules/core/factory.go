02947d6e701d1b1423557d51f171cf509cf0a38a
Erkenbend <ad.game-adress@hotmail.fr>
2025-10-10 21:53:02 +0200

    feat: Make JSON output flatter and add more infos from the rule

--- rules/core/factory.go
+++ rules/core/factory.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"fmt"
+
 	"github.com/Marcel2603/tfcoach/internal/types"
 )
 
@@ -9,9 +11,26 @@ const (
 )
 
 var (
-	rules = []types.Rule{NamingConventionRule(), FileNamingRule(), RequiredProviderMustBeDeclaredRule()}
+	rules   = []types.Rule{NamingConventionRule(), FileNamingRule(), RequiredProviderMustBeDeclaredRule()}
+	ruleMap = mapRules(rules)
 )
 
 func All() []types.Rule {
 	return rules
+}
+
+func FindById(id string) (types.Rule, error) {
+	rule, ok := ruleMap[id]
+	if !ok {
+		return nil, fmt.Errorf("no rule found for ID %s", id)
+	}
+	return rule, nil
+}
+
+func mapRules(rulesList []types.Rule) map[string]types.Rule {
+	result := make(map[string]types.Rule)
+	for _, rule := range rulesList {
+		result[rule.ID()] = rule
+	}
+	return result
 }
