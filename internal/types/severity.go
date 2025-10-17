d0bdff1bef265fe93f1716619896667af205c411
Erkenbend <adri.w51@hotmail.fr>
2025-10-17 20:52:01 +0200

    feat: Add color to 'pretty' output

--- internal/types/severity.go
+++ internal/types/severity.go
@@ -3,6 +3,8 @@ package types
 import (
 	"cmp"
 	"encoding/json"
+
+	"github.com/fatih/color"
 )
 
 type Severity struct {
@@ -16,6 +18,20 @@ func (s Severity) Cmp(other Severity) int {
 
 func (s Severity) String() string {
 	return s.Str
+}
+
+func (s Severity) ColoredString() string {
+	str := s.String()
+	switch s.Priority {
+	case 1:
+		return color.HiRedString(str)
+	case 2:
+		return color.HiYellowString(str)
+	case 3:
+		return color.HiWhiteString(str)
+	default:
+		return str
+	}
 }
 
 func (s Severity) MarshalJSON() ([]byte, error) {
