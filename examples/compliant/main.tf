5d4d60de37ac32f4ac3971d327b7ce9fba1e3327
Erkenbend <adri.w51@hotmail.fr>
2026-03-04 17:17:16 +0100

    docs: add examples for rule resource_parameter_order

--- examples/compliant/main.tf
+++ examples/compliant/main.tf
@@ -1,1 +1,21 @@
 resource "terraform_data" "test" {}
+
+resource "aws_instance" "web1" {
+  count = 1
+  ami = 4321
+  lifecycle {
+    ignore_changes = [tags]
+  }
+  depends_on = []
+}
+
+resource "aws_instance" "web2" {
+  ami = 1234
+  availability_zone = "custom-az"
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      max_price = 0.002
+    }
+  }
+}
